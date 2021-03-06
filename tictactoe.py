ed2ffe1d8804a2f0855aa6917ea407973c33f923
adeena210 <57496912+adeena210@users.noreply.github.com>
2021-03-07 02:00:45 +0400

    Update tictactoe.py
    Removed System
    Changed opponent's ChooseRandom to ChooseMove

--- tictactoe.py
+++ tictactoe.py
@@ -234,6 +234,28 @@ class Board:
                     successor[x][y] = symbol
                     successors.append(successor)
         return successors
+        
+    def getTrainingData(self,playthrough,player):
+        trainingData = []
+
+        for i in range(0,len(playthrough)):
+            if(self.check_completion(playthrough[i])):
+                if(self.assign_winner(playthrough[i]) == player.symbol):
+                    trainingData.append([self.getFeatures(playthrough[i]), 100])
+                elif(self.assign_winner(playthrough[i]) == 0):
+                    trainingData.append([self.getFeatures(playthrough[i]), 0])
+                else:
+                    trainingData.append([self.getFeatures(playthrough[i]), -100])
+            else:
+                if i+2 >= len(playthrough):
+                    if(self.assign_winner(playthrough[len(playthrough)-1]) == 0):
+                        trainingData.append([self.getFeatures(playthrough[i]), 0])
+                    else:
+                        trainingData.append([self.getFeatures(playthrough[i]), -100])
+                else:
+                    trainingData.append([self.getFeatures(playthrough[i]), player.evaluateBoard(playthrough[i+2])])
+
+        return trainingData
 
 class Player:
     def __init__(self,board,weights,symbol):
@@ -299,55 +321,7 @@ class Player:
                 bestMove = successor
                 
         self.board.setBoard(bestMove)
-
-    #this function just looks at successor 0,
-    def chooseRandom(self):
-        successors = self.board.getSuccessors(self.symbol)
-        bestMove = successors[0]
-            
-        randomBoard = successors[random.randint(0,len(successors)-1)]
-        self.board.setBoard(randomBoard)
-
-class System:
-    def __init__(self,board,weights,symbol):
-        self.board = board
-        self.weights = weights
-        self.symbol = symbol
-        self.checker = Board()
-
-    def evaluateBoard(self,board):
-        x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12 = self.board.getFeatures(board)
-        w0,w1,w2,w3,w4,w5,w6,w7,w8,w9,w10,w11,w12 = self.weights
-
-        return w0 + w1*x1 + w2*x2 + w3*x3 + w4*x4 + w5*x5 + w6*x6 + w7*x7 + w8*x8 + w9*x9 + w10*x10 + w11*x11 + w12*x12
-
-    def setWeights(self, weights):
-        self.weights = weights
-
-    def setSymbol(self, symbol):
-        self.weights = weights
-
-    def getTrainingData(self,playthrough):
-        trainingData = []
-
-        for i in range(0,len(playthrough)):
-            if(self.checker.check_completion(playthrough[i])):
-                if(self.checker.assign_winner(playthrough[i]) == self.symbol):
-                    trainingData.append([self.checker.getFeatures(playthrough[i]), 100])
-                elif(self.checker.assign_winner(playthrough[i]) == 0):
-                    trainingData.append([self.checker.getFeatures(playthrough[i]), 0])
-                else:
-                    trainingData.append([self.checker.getFeatures(playthrough[i]), -100])
-            else:
-                if i+2 >= len(playthrough):
-                    if(self.checker.assign_winner(playthrough[len(playthrough)-1]) == 0):
-                        trainingData.append([self.checker.getFeatures(playthrough[i]), 0])
-                    else:
-                        trainingData.append([self.checker.getFeatures(playthrough[i]), -100])
-                else:
-                    trainingData.append([self.checker.getFeatures(playthrough[i]), self.evaluateBoard(playthrough[i+2])])
-
-        return trainingData
+    
 
 
 b = Board()
@@ -355,21 +329,21 @@ b.create_board()
 b.print_board()
 
 weights1 = (.3,.3,.3,.3,.3,.3,.3,.3,.3,.3,.3,.3,.3)
-weights2 = (.3,.3,.3,.3,.3,.3,.3,.3,.3,.3,.3,.3,.3)
+weights2 = (.3,-10,10,-10,10,-10,10,-10,10,-10,10,-10,10)
 
 learner = Player(b, weights1, 'X')
 opponent = Player(b, weights2, 'O')
 
 opponent.setLearningRate(0)
 
-system1 = System(b, weights1, 'X')
-system2 = System(b, weights2, 'O')
+# system1 = System(b, weights1, 'X')
+# system2 = System(b, weights2, 'O')
 
 X_wins = 0
 O_wins = 0
 tie = 0
 
-for i in range(0,10000):
+for i in range(0,10):
     b = Board()
     learner.setBoard(b)
     opponent.setBoard(b)
@@ -380,7 +354,7 @@ for i in range(0,10000):
         if b.check_completion():
             #b.print_board()
             break
-        opponent.chooseRandom()
+        opponent.chooseMove()
         b.print_board()
 
 
@@ -396,11 +370,9 @@ for i in range(0,10000):
         print ("The game is a tie.")
         tie += 1
 
-    system1.setWeights(learner.getWeights())
-    system2.setWeights(opponent.getWeights())
 
-    learner.LMS(b.getPlaythrough(),system1.getTrainingData(b.getPlaythrough()))
-    opponent.LMS(b.getPlaythrough(),system2.getTrainingData(b.getPlaythrough()))
+    learner.LMS(b.getPlaythrough(),b.getTrainingData(b.getPlaythrough(),learner))
+    #opponent.LMS(b.getPlaythrough(),b.getTrainingData(b.getPlaythrough(),learner))
 
     print ("X won " + str(X_wins) + " games.")
     print ("O won " + str(O_wins) + " games.")
@@ -410,5 +382,3 @@ for i in range(0,10000):
     print("DONEEEEE")
     print ("L", learner.getWeights())
     print ("O", opponent.getWeights())
-
-
